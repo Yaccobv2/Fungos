82c01844beb13ea337d57a4e947ccc4967faced6
Yaccobv2 <jakub.codogni@gmail.com>
2021-03-31 16:30:30 +0200

    Code update
    added new file functions.py to make it more transparent

---
+++ functions.py
@@ -0,0 +1,69 @@
+import cv2
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.model_selection
+import tensorflow as tf
+
+
+def resize(img):
+    img = cv2.resize(img, (50, 50))
+    # plt.imshow(img)
+    return img
+
+
+def readData(dir,number_of_images):
+    images = []
+    labels = []
+    classes = []
+    print("Reading data...")
+    for folder in os.listdir(dir):
+        counter = 0
+        for imgs in os.listdir(dir + "/" + folder):
+            img = cv2.imread(dir + "/" + folder + "/" + imgs, )
+
+            images.append(img)
+            labels.append(str(folder))
+            print("len(zdjecia): ", len(images))
+            counter += 1
+            if folder not in classes:
+                classes.append(folder)
+            if counter >= number_of_images:
+                break
+    return images, labels, classes
+
+
+def createNpArray(input_array):
+    output_array = np.array(input_array)
+    return output_array
+
+
+def splitDataIntoContainers(data, labels, size):
+    data1, data2, label1, label2 = sklearn.model_selection.train_test_split(data, labels, test_size=size)
+    return data1, data2, label1, label2
+
+
+def oneHotEncoding(labels, number_of_classes):
+    encoded_labels = tf.keras.utils.to_categorical(labels, number_of_classes)
+    return encoded_labels
+
+
+def createNeuralNetwork(input_shape, number_of_classes):
+    model = tf.keras.Sequential()
+
+    model.add(tf.keras.layers.Conv2D(64, (5, 5), activation='relu', input_shape=input_shape))
+    model.add(tf.keras.layers.Conv2D(64, (5, 5), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(0.5))
+
+    model.add(tf.keras.layers.Flatten())
+
+    model.add(tf.keras.layers.Dense(100, activation='relu'))
+    model.add(tf.keras.layers.Dense(number_of_classes, activation='softmax'))
+
+    model.compile(tf.keras.optimizers.Adam(lr=0.001), loss='categorical_crossentropy', metrics='accuracy')
+
+    return model
--- main.py
+++ main.py
@@ -1,95 +1,48 @@
-import tensorflow as tf
+import functions
 import numpy as np
-import cv2
-import os
-import matplotlib
-import sklearn.model_selection
 
-liczba_klas=8
-train_data = []
-train_label =[]
-validation_data = []
-validation_label = []
-test_data = []
-test_label = []
-
-zdjecia = []
-labele = []
-klasy = []
-
-def resize(img):
-    img = cv2.resize(img, (50, 50))
-    return img
-
-print("Zaczynam wczytywac")
-for folder in os.listdir("C:/Users/Mateusz/Documents/GitHub/Fungos_dataset/Mushrooms"):
-    licznik=0
-    for imgs in os.listdir("C:/Users/Mateusz/Documents/GitHub/Fungos_dataset/Mushrooms"+"/"+folder):
-        img = cv2.imread("C:/Users/Mateusz/Documents/GitHub/Fungos_dataset/Mushrooms"+"/"+folder+"/"+imgs, )
-
-        zdjecia.append(img)
-        labele.append(str(folder))
-        print("len(zdjecia): ", len(zdjecia))
-        licznik += 1
-        if folder not in klasy:
-            klasy.append(folder)
-        if(licznik>=100):
-            break
+dir = "E:/GitHub/fungos_libs/Mushrooms"
 
 
-print()
+images, labels, classes = functions.readData(dir,500)
 
-print("Liczba klas: ", len(klasy))
-print("Liczba obrazow: ", len(zdjecia))
-print("Liczba labeli: ", len(labele))
-print()
-print("klasy: :", klasy)
-print("labele: ", labele)
-print()
+print("Number of classes: ", len(classes))
+print("Number of images: ", len(images))
+print("Number of labels: ", len(labels))
+print("clases: :", classes)
+print("labels: ", labels)
 
-zdjecia = np.array(zdjecia)
-klasy = np.array(klasy)
+images = functions.createNpArray(images)
+classes = functions.createNpArray(classes)
 
-print()
-print(zdjecia[0].shape)
-zdjecia = list(map(resize, zdjecia))
-print(zdjecia[0].shape)
-print()
+print(images[0].shape)
+images = list(map(functions.resize, images))
+print(images[0].shape)
 
-train_data, test_data, train_label,  test_label = sklearn.model_selection.train_test_split(zdjecia, labele, test_size=0.2)
-train_data, validation_data, train_label, validation_label = sklearn.model_selection.train_test_split(train_data, train_label, test_size=0.1)
+train_data, test_data, train_label, test_label = functions.splitDataIntoContainers(images, labels, 0.2)
+train_data, validation_data, train_label, validation_label = functions.splitDataIntoContainers(train_data, train_label,
+                                                                                               0.1)
 
-print("Liczba zdjec treningowych: ", len(train_data))
-print("Liczba zdjec testowych: ", len(test_data))
-print("Liczba zdjec walidacyjnych: ", len(validation_data))
+print("Number of training images: ", len(train_data))
+print("Number of testing images: ", len(test_data))
+print("Number f validating data: ", len(validation_data))
 
-train_label=tf.keras.utils.to_categorical(train_label, len(klasy))
-test_label=tf.keras.utils.to_categorical(test_label, len(klasy))
-validation_label=tf.keras.utils.to_categorical(validation_label, len(klasy))
+# prepare labels to create output layer
+train_label = functions.oneHotEncoding(train_label, len(classes))
+test_label = functions.oneHotEncoding(test_label, len(classes))
+validation_label = functions.oneHotEncoding(validation_label, len(classes))
 
-model = tf.keras.Sequential()
+# create neural network
+model = functions.createNeuralNetwork((50, 50, 3), len(classes))
 
-model.add(tf.keras.layers.Conv2D(64, (5, 5), activation='relu', input_shape= (50, 50, 3)))
-model.add(tf.keras.layers.Conv2D(64, (5, 5), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.Dropout(0.5))
-
-model.add(tf.keras.layers.Flatten())
-
-model.add(tf.keras.layers.Dense(100, activation='relu'))
-model.add(tf.keras.layers.Dense(len(klasy), activation='softmax'))
-
-model.compile(tf.keras.optimizers.Adam(lr=0.001), loss='categorical_crossentropy', metrics='accuracy')
-#print(model.summary())
+# print(model.summary())
 print("########")
 print(len(train_data))
 print(len(train_label))
 print("########")
 
-x = model.fit(np.array(train_data), np.array(train_label), batch_size=30, epochs=10, steps_per_epoch=20, validation_data=(np.array(validation_data), np.array(validation_label)), shuffle=True)
+x = model.fit(np.array(train_data), np.array(train_label), batch_size=60, epochs=30, steps_per_epoch=20,
+              validation_data=(np.array(validation_data), np.array(validation_label)), shuffle=True)
 
 y = model.evaluate(np.array(test_data), np.array(test_label))
 
